f5432ebd52c8219fa85062fa6fd0874697dcbab3
Isabella Imburgia <iimburgia11@gmail.com>
2026-02-27 20:05:29 -0500

    Update exploratory_analysis_day1.py
    code to find estimated R0 and plot against curve from data

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -1,10 +1,11 @@
-#%%
-from datetime import date
 
+from datetime import date
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-
+# AI Usage Statement: ChatGPT 5.2 for debugging and error checking 
 # Load the data
 data = pd.read_csv('C:\\Users\\ajq2af\\OneDrive - University of Virginia\\Documents\\UVA\\BME 2315\\module2\\Module-2-Epidemics-SIR-Modeling\\Data\\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 
@@ -34,7 +35,7 @@ class Virus:
                 Virus(day, active_cases)
 
 print(data)
-#%% Make a plot of the active cases over time
+
 plt.figure(figsize=(10, 6))
 plt.plot(data['day'], data['active reported daily cases'], marker='o')
 plt.title('Active Cases of Mystery Virus Over Time')
@@ -42,5 +43,34 @@ plt.xlabel('Day')
 plt.ylabel('Number of Active Cases')
 plt.xticks(rotation=45)
 plt.grid()
+plt.tight_layout()
+plt.show()
+
+#calculate the R0 value from early data csv file (first 45 days)
+def eponential_growth(t,r):
+    return np.exp(r*t)
+
+x_data = data['day'].values.astype(float)
+y_data = data['active reported daily cases'].values.astype(float)
+popt, pcov = curve_fit(eponential_growth, x_data, y_data)
+r_fit = popt[0]
+
+D = 5 # estimated infection period from data 
+
+r0 = np.exp(r_fit * D)
+
+print("Estimated R0 value:", r0)
+# add the fitted curve to the plot
+y_fitted = eponential_growth(x_data, r_fit)
+# plot fitted curve and actual data
+plt.figure(figsize=(10, 6))
+plt.plot(x_data, y_data, label=' Actual Data')
+plt.plot(x_data, y_fitted, color = "red", label = "Estimated Curve")
+plt.title('Exponential Growth Fit to Active Cases')
+plt.xlabel('Day')
+plt.ylabel('Number of Active Cases')
+plt.xticks(rotation=45)
+plt.grid()
+plt.legend()
 plt.tight_layout()
 plt.show()
