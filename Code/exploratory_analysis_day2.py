ffe220f1364d88edc6b8de3af7f4c69ea33e1c55
Isabella Imburgia <iimburgia11@gmail.com>
2026-03-08 16:09:57 -0400

    Update exploratory_analysis_day2.py
    isabella - starting to figure out euler approx for SEIR model

--- Code/exploratory_analysis_day2.py
+++ Code/exploratory_analysis_day2.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv('C:\\Users\\ajq2af\\OneDrive - University of Virginia\\Documents\\UVA\\BME 2315\\module2\\Module-2-Epidemics-SIR-Modeling\\Data\\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 #%%
 # We have day number, date, and active cases. We can use the day number and active cases to fit an exponential growth curve to estimate R0.
 # Let's define the exponential growth function
@@ -19,3 +19,46 @@ def exponential_growth(t, r):
 # Approximate R0 using this fit
 
 # Add the fit as a line on top of your scatterplot.
+
+#%% Euler's Method Approximation of SEIR
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+data = pd.read_csv('C:\\Users\\ajq2af\\OneDrive - University of Virginia\\Documents\\UVA\\BME 2315\\module2\\Module-2-Epidemics-SIR-Modeling\\Data\\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+
+def euler_seir(beta, sigma, gamma, S0, E0, I0, R0, timepoints, N):
+    S = []
+    E = []
+    I = []
+    R = []
+
+    S.append(S0)
+    E.append(E0)   
+    I.append(I0)
+    R.append(R0)
+
+    for t in timepoints:
+        dS = -beta * S[-1] * I[-1] / N
+        dE = beta * S[-1] * I[-1] / N - sigma * E[-1]
+        dI = sigma * E[-1] - gamma * I[-1]
+        dR = gamma * I[-1]
+
+        S_new = S[t+1] = S[-1] + dS
+        E_new = E[t+1] = E[-1] + dE
+        I_new = I[t+1] = I[-1] + dI
+        R_new = R[t+1] = R[-1] + dR
+
+        S.append(S_new)
+        E.append(E_new)
+        I.append(I_new)
+        R.append(R_new)
+        
+    return S, E, I, R
+
+timepoints = data["day"].to_numpy()
+beta = 0.3
+sigma = 0.1
+gamma = 0.05
+N = 1000000
+
